4c9ddab6189550b4d9ea69e3242a553a37ea7ca4
Han <leook0209@gmail.com>
2018-01-28 00:18:09 +0800

    convert html to ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'BlockBoard' });
+});
+
+router.get('/mine', function(req, res, next) {
+  res.render('mine', { title: 'BlockBoard' });
+});
+
+router.get('/ledger', function(req, res, next) {
+  res.render('ledger', { title: 'BlockBoard' });
 });
 
 module.exports = router;
