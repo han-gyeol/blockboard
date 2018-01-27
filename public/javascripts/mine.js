e35857cefaee28dfb382de046baba8bd18629613
calyeefornia <e0175177@u.nus.edu>
2018-01-28 07:15:42 +0800

    added loading function to mine page

--- public/javascripts/mine.js
+++ public/javascripts/mine.js
@@ -1,4 +1,6 @@
 const letters = document.querySelectorAll(".classes");
+const letterBlk = document.querySelectorAll("#letterBlk");
+const loader = document.querySelectorAll(".loader");
 let oldResponse;
 
 let ajax = () => {
@@ -7,6 +9,8 @@ let ajax = () => {
     if (this.readyState == 4 && this.status == 200) {
      let newResponse = this.responseText;
      if(oldResponse == undefined){
+        letterBlk[0].style.display = "block";
+        loader[0].style.display = "none";
      	oldResponse = newResponse;
      	let parsedResponse = JSON.parse(oldResponse);
      	for(let i=0; i<parsedResponse.length;i++){
@@ -32,34 +36,34 @@ let ajax = () => {
   xhttp.send();
 }
 
-let difficulty = 1;
-let mineBlock = (name, char, index) => {
-	let latestBlock = <%-JSON.stringify(latest_block)%>;
-	let newBlock = {
-		"_id": parseInt(latestBlock._id)+1,
-		"previousHash": latestBlock.hash,
-		"hash": "temp",
-		"timestamp": Date.now(),
-		"nonce": 0,
-		"data": {
-			"name": name,
-			"char": char,
-			"index": index,
-			"display": 1
-		}
-	}
-	while (newBlock.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-		newBlock.nonce++;
-		newBlock.hash = calculateHash(newBlock);
-	}
+// let difficulty = 1;
+// let mineBlock = (name, char, index) => {
+// 	let latestBlock = <%-JSON.stringify(latest_block)%>;
+// 	let newBlock = {
+// 		"_id": parseInt(latestBlock._id)+1,
+// 		"previousHash": latestBlock.hash,
+// 		"hash": "temp",
+// 		"timestamp": Date.now(),
+// 		"nonce": 0,
+// 		"data": {
+// 			"name": name,
+// 			"char": char,
+// 			"index": index,
+// 			"display": 1
+// 		}
+// 	}
+// 	while (newBlock.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+// 		newBlock.nonce++;
+// 		newBlock.hash = calculateHash(newBlock);
+// 	}
 
-	console.log("BLOCK MINED: " + newBlock.hash);
-}
+// 	console.log("BLOCK MINED: " + newBlock.hash);
+// }
 
 let calculateHash = (block) => {
 	return sha256(block._id + block.previousHash + block.hash + block.timestamp + JSON.stringify(block.data) + block.nonce).toString();
 }
 
 window.onload=function(){
-	setInterval('ajax()',10000);
+	setInterval('ajax()',1000);
 }
