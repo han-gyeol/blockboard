f44f5fb5f4b0471e6e6014ae684a723dd064dcf6
Han <leook0209@gmail.com>
2018-01-28 13:30:35 +0800

    change difficulty

--- public/javascripts/ledger.js
+++ public/javascripts/ledger.js
@@ -58,7 +58,7 @@ let ajax = () => {
         // let indexChange = checkArrayDifferent(parsedOldResponse, parsedNewResponse);
         // let temp = parsedResponse[indexChange];
         let temp = parsedResponse[id];
-     	createRow(tmep._id, temp.hash, temp.data.name, temp.data.char, temp.data.index, temp.timestamp);
+     	createRow(temp._id, temp.hash, temp.data.name, temp.data.char, temp.data.index, temp.timestamp);
 
      }
     }
